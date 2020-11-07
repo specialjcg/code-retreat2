072f9973a973e5524009c0fb9f1c3e92bf331a43
jcgouleau <jean-charles.gouleau@savoye.com>
2020-11-07 14:30:50 +0100

    first commit

---
+++ codeRetreat.spec.ts
@@ -0,0 +1,87 @@
+class CellsLive {
+    next(listCellsNeighbours: DeadCell[]){
+        const numberOfLivingCells=listCellsNeighbours.filter(cell=>cell instanceof CellsLive ).length;
+        if (numberOfLivingCells===2||numberOfLivingCells===3) return new CellsLive();
+        return new DeadCell()
+
+    }
+}
+
+class DeadCell {
+    next(listCellsNeighbours: CellsLive[]){
+        const numberOfLivingCells=listCellsNeighbours.filter(cell=>cell instanceof CellsLive ).length;
+        if (numberOfLivingCells===3) return new CellsLive();
+        return new DeadCell();
+    }
+}
+
+describe('test jeu de la vie', function () {
+    it('Should return dead when   less than two living neighbours', function () {
+        //Given
+        const cellsLive= new CellsLive();
+        const listCellsNeighbours=[new DeadCell()];
+
+        //when
+        const nextCell=cellsLive.next(listCellsNeighbours);
+
+        //then
+        expect(nextCell).toBeInstanceOf(DeadCell)
+    });
+    it('Should  stay alive with two living neighbours', function () {
+        //Given
+        const cellsLive= new CellsLive();
+        const listCellsNeighbours=[new CellsLive(),new CellsLive()];
+
+        //when
+        const nextCell=cellsLive.next(listCellsNeighbours);
+
+        //then
+        expect(nextCell).toBeInstanceOf(CellsLive)
+    });
+
+    it('Should  stay alive with three living neighbours', function () {
+        //Given
+        const cellsLive= new CellsLive();
+        const listCellsNeighbours=[new CellsLive(),new CellsLive(),new CellsLive()];
+
+        //when
+        const nextCell=cellsLive.next(listCellsNeighbours);
+
+        //then
+        expect(nextCell).toBeInstanceOf(CellsLive)
+    });
+    it('Should died when more than three living neighbours', function () {
+        //Given
+        const cellsLive= new CellsLive();
+        const listCellsNeighbours=[new CellsLive(),new CellsLive(),new CellsLive(),new CellsLive()];
+
+        //when
+        const nextCell=cellsLive.next(listCellsNeighbours);
+
+        //then
+        expect(nextCell).toBeInstanceOf(DeadCell)
+    });
+
+    it('Should dead cells with  three living neighbours become alive', function () {
+        //Given
+        const cellsDead= new DeadCell();
+        const listCellsNeighbours=[new CellsLive(),new CellsLive(),new CellsLive()];
+
+        //when
+        const nextCell=cellsDead.next(listCellsNeighbours);
+
+        //then
+        expect(nextCell).toBeInstanceOf(CellsLive)
+    });
+    it('Should stay dead  when do not have  three living neighbours ', function () {
+        //Given
+        const cellsDead= new DeadCell();
+        const listCellsNeighbours=[new CellsLive(),new CellsLive()];
+
+        //when
+        const nextCell=cellsDead.next(listCellsNeighbours);
+
+        //then
+        expect(nextCell).toBeInstanceOf(DeadCell)
+    });
+});
